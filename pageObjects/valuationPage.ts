6763688d8d0f00001be3b4256ee49576142c26db
Laura Vanessa Hernández Benítez <avivanessa26@gmail.com>
2024-09-17 17:00:26 -0300

    add xtra steps

--- pageObjects/valuationPage.ts
+++ pageObjects/valuationPage.ts
@@ -5,7 +5,7 @@ export default class ValuationPage{
     readonly page: Page
     readonly clientInput: Locator
     readonly actionsButton: Locator
-    readonly addProjectButton: Locator
+    readonly addProjectTitle: Locator
     readonly fileInput: Locator
     readonly nextButton: Locator
     readonly folderInput: Locator
@@ -14,13 +14,16 @@ export default class ValuationPage{
     readonly projectName: Locator
     readonly projectList: Locator
     readonly opinionDate: Locator
-    readonly main: Locator
+    readonly valuationDate: Locator
+    readonly allProcedures: Locator
+    readonly addProjectButton: Locator
+    readonly monthPicker: Locator
 
     constructor(page: Page){
         this.page = page
         this.clientInput = this.page.locator('input.ant-select-selection-search-input')
         this.actionsButton = this.page.locator('.ant-space-item:has-text("Actions")')
-        this.addProjectButton = this.page.getByText('Add Project')
+        this.addProjectTitle = this.page.getByText('Add Project')
         this.fileInput = this.page.locator('input[type="file"]')
         this.nextButton = this.page.locator('//button[span[text()="Next"]]')
         this.folderInput = this.page.locator('#rc_select_4')
@@ -29,6 +32,10 @@ export default class ValuationPage{
         this.projectList = this.page.locator('rc-virtual-list-holder-inner')
         this.projectName = this.page.locator('//div[text()="GD"]') // --> this locator should parameterize the project name somehow.
         this.opinionDate = this.page.locator('input[name="opinionDate"]')
+        this.valuationDate = this.page.locator('input[name="valuationDate"]')
+        this.allProcedures = this.page.getByLabel('All Procedures')
+        this.addProjectButton = this.page.locator('//button[@type="submit"]/span[contains(text(),"Add Project")]')
+        this.monthPicker = this.page.locator('//button[@class="ant-picker-month-btn"]')
     }
 
     async selectClient(client: string){ 
@@ -38,11 +45,13 @@ export default class ValuationPage{
     }
 
     async addProject(){
-        const filePath = './test-data/DNAV_Client_Data_Template 1.xlsm'
-        const date = randomFutureDayCurrentMonth()
+        const clientDataFilePath = './test-data/DNAV_Client_Data_Template 1.xlsm'
+        const counterPartyFilePath = './test-data/DNAV_CounterParty_Data_Template 1 1.xlsm'
+        const opinionDate = randomFutureDayCurrentMonth()
+
         await this.actionsButton.click() 
-        await this.addProjectButton.click()
-        await this.fileInput.setInputFiles(filePath)
+        await this.addProjectTitle.click()
+        await this.fileInput.setInputFiles(clientDataFilePath)
         await this.nextButton.click()
         await this.folderInput.click()
         await this.folderName.click()
@@ -51,6 +60,11 @@ export default class ValuationPage{
         await this.projectInput.click()
         await this.opinionDate.click()
         await this.opinionDate.click()
-        await this.page.getByText(date, { exact: true }).click()
+        await this.page.getByText(opinionDate, { exact: true }).click()
+        await this.valuationDate.click()
+        await this.monthPicker.click()
+        //await this.allProcedures.click()
+        //await this.fileInput.setInputFiles(counterPartyFilePath)
+        //await this.addProjectButton.click()
     }
 }
